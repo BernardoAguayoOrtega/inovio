b8aa202195d6d74e6b0d05e9bd467342bcb31f26
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-02 23:00:09 -0500

    add js

---
+++ public/js/modal.js
@@ -0,0 +1,42 @@
+//global variable
+let modalBg = document.querySelector('.modal-bg');
+let putContent = document.querySelector('.send')
+
+//Function to show up modal
+function showUp() {
+    let modalBg = document.querySelector('.modal-bg');
+    modalBg.classList.add("active")
+}
+
+//create variable
+let modalClose = document.querySelector('.modal-close');
+
+//add event listener to x element to close modal 
+modalClose.addEventListener('click', () => {
+    modalBg.classList.remove("active")
+})
+
+//change welcome label content 
+let changeContent = (content) => {
+    document.getElementById('welcome-label').textContent = `Welcome ${content}`
+}
+
+//get name and use change function to change the text content
+let getName = () => {
+    let name = document.getElementById("name").value
+    changeContent(name)
+}
+
+//add event lister to send button
+putContent.addEventListener('click', () => {
+    //variable to get email data
+    let email = document.getElementById("email").value
+        //validation for email
+    if (email === "") {
+        window.alert("Complete all fields")
+            // make changes
+    } else {
+        getName()
+        modalBg.classList.remove("active")
+    }
+})
--- public/js/showModal.js
+++
@@ -1,4 +0,0 @@
-function showUp() {
-    let modelBg = document.querySelector('.modal-bg');
-    modelBg.classList.add("active")
-}
